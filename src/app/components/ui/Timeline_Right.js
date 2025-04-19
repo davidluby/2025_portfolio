a4703f16753648cedcdcff279effdfd293c6aca6
davidluby <davidluby273@gmail.com>
2025-04-19 18:37:01 -0400

    testing mobile image rotation

--- src/app/components/ui/Timeline_Right.js
+++ src/app/components/ui/Timeline_Right.js
@@ -34,7 +34,7 @@ const Timeline_Element = ({ content }) => {
           <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" />
       </svg>
       </div>
-      <div className="timeline-start text-start">
+      <div className="timeline-start text-start md:text-end">
         <time className="font-mono italic">{content.year}</time>
         <div className="text-lg font-black">{content.title}</div>
         {content.text}
